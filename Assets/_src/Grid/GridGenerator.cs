4f7466800a95e8738f343a170475d3f2c11e0475
Erkan <etaylan13@gmail.com>
2023-02-05 01:22:09 +0300

    spawn grid debug objects

---
+++ Assets/_src/Grid/GridGenerator.cs
@@ -0,0 +1,55 @@
+﻿using _src.Grid.Debug;
+using UnityEngine;
+
+namespace _src.Grid
+{
+    public class GridGenerator
+    {
+        private readonly GridCell[,] cells;
+        private readonly float cellSize;
+        private readonly int height;
+        private readonly int width;
+
+        public GridGenerator(int width, int height, float cellSize)
+        {
+            this.width = width;
+            this.height = height;
+            this.cellSize = cellSize;
+            cells = new GridCell[width, height];
+        }
+
+        public GridCell[,] CreateCells()
+        {
+            for (var x = 0; x < width; x++)
+            {
+                for (var z = 0; z < height; z++)
+                {
+                    cells[x, z] = GenerateCell(x, z);
+                }
+            }
+
+            return cells;
+        }
+
+        public void SpawnDebugObjects(Transform prefab, Transform parent = null)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                for (var z = 0; z < height; z++)
+                {
+                    var position = new CellPosition(x, z);
+                    Transform debug = Object.Instantiate(prefab, position.ToWorldPosition(cellSize), Quaternion.identity);
+                    debug.SetParent(parent);
+                    var component = debug.GetComponent<GridDebugMono>();
+                    component.SetGridCell(cells[x, z]);
+                }
+            }
+        }
+
+        private static GridCell GenerateCell(int x, int z)
+        {
+            var positoin = new CellPosition(x, z);
+            return new GridCell(positoin);
+        }
+    }
+}
---
+++ Assets/_src/Grid/GridManagerMono.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace _src.Grid
+{
+    public class GridManagerMono : MonoBehaviour
+    {
+        [SerializeField]
+        private Data data;
+
+        public GridCell[,] Cells { get; private set; }
+
+        private void Start()
+        {
+            var generator = new GridGenerator(data.width, data.height, data.cellSize);
+            Cells = generator.CreateCells();
+            generator.SpawnDebugObjects(data.debugPrefab, data.debugObjectsParent);
+        }
+
+        private void OnValidate()
+        {
+            if (data.debugObjectsParent == null)
+            {
+                return;
+            }
+
+            data.debugObjectsParent.gameObject.SetActive(data.showDebugObjects);
+        }
+
+        [Serializable]
+        public class Data
+        {
+            public Transform debugPrefab;
+            public Transform debugObjectsParent;
+            public bool showDebugObjects;
+
+            [Header("Values")]
+            public int width;
+            public int height;
+            public float cellSize;
+        }
+    }
+}
