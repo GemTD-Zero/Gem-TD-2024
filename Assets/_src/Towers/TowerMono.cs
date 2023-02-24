bf2ca484d7581c99abeecdcc36b539bf8ff89e1b
Erkan <etaylan13@gmail.com>
2023-02-24 19:11:46 +0300

    look at closest enemy

--- Assets/_src/Towers/TowerMono.cs
+++ Assets/_src/Towers/TowerMono.cs
@@ -1,14 +1,53 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace _src.Towers
 {
     public class TowerMono : MonoBehaviour
     {
+        [SerializeField]
+        private List<Transform> enemies;
+
         private Transform tower;
+
+        [SerializeField]
+        private float attackRange  = 3f;
 
         private void Start()
         {
             tower = transform.GetChild(0);
+        }
+
+        private void Update()
+        {
+            (Transform closestEnemy, float distance) = FindClosestTarget();
+            if (closestEnemy != null)
+            {
+                // Vector3 lookingVector = closestTarget.position - transform.position;
+                // lookingVector.y = transform.position.y;
+
+                if (attackRange > distance)
+                {
+                    transform.LookAt(closestEnemy);    
+                }
+            }
+        }
+
+        private (Transform enemy, float distance) FindClosestTarget()
+        {
+            var minimum = float.MaxValue;
+            Transform closestEnemy = null;
+            foreach (Transform enemy in enemies)
+            {
+                float distance = (tower.position - enemy.position).sqrMagnitude;
+                if (distance < minimum)
+                {
+                    minimum = distance;
+                    closestEnemy = enemy;
+                }
+            }
+
+            return (closestEnemy, Mathf.Sqrt(minimum));
         }
 
         public void ChangeTower(Transform newTower)
